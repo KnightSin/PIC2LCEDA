eac02c0bd03587b1b73a9d6ad69f052d5116eb3f
矛盾聚合体 <42244533+KnightSin@users.noreply.github.com>
2020-07-19 07:55:39 +0800

    2020年7月19日07点55分

--- PIC2LCEDA.py
+++ PIC2LCEDA.py
@@ -5,21 +5,30 @@ import numpy as np
 from numpy import mat
 
 # 输入参数
-test_f = int(input('测试模式?(1, yes; 0, no):'));
-if test_f == 1:
-    x_size = 50;
-    y_size = 50;
-    layer = 7;
-    sourcepath = 'C:\\Users\\sora\\Desktop\\';
-    sourcename = 'test3.bmp';
-    invert_f = 1;
-else :
-    x_size = int(input('请输入X最大尺寸(mm): '));
-    y_size = int(input('请输入y最大尺寸(mm): '));
-    layer = int(input('请输入所在层(1，顶层；2，底层；3，顶层丝印层；4，底层丝印层；5，顶层焊盘层；6，底层焊盘层；7，顶层阻焊层；8，底层阻焊层；10，边框层；11，文档层): '));
-    sourcepath = input('请输入源文件路径(示例：C:\\Users\\sora\\Desktop\\): ');
-    sourcename = input('请输入源文件名称(示例：test4.bmp): ');
-    invert_f = int(input('图像取反?(0,不取反; 1,取反): '));
+# test_f = int(input('测试模式?(1, yes; 0, no):'));
+# if test_f == 1:
+#     x_size = 50;
+#     y_size = 50;
+#     layer = 7;
+#     sourcepath = 'C:\\Users\\sora\\Desktop\\';
+#     sourcename = 'test3.bmp';
+#     invert_f = 1;
+# else :
+#     x_size = int(input('请输入X最大尺寸(mm): '));
+#     y_size = int(input('请输入y最大尺寸(mm): '));
+#     layer = int(input('请输入所在层(1，顶层；2，底层；3，顶层丝印层；4，底层丝印层；5，顶层焊盘层；6，底层焊盘层；7，顶层阻焊层；8，底层阻焊层；10，边框层；11，文档层): '));
+#     sourcepath = input('请输入源文件路径(示例：C:\\Users\\sora\\Desktop\\): ');
+#     sourcename = input('请输入源文件名称(示例：test4.bmp): ');
+#     invert_f = int(input('图像取反?(0,不取反; 1,取反): '));
+#     test_f = int(input('测试模式?(1, yes; 0, no):'));
+
+# 输入参数
+x_size = int(input('请输入X最大尺寸(mm): '));
+y_size = int(input('请输入y最大尺寸(mm): '));
+layer = int(input('请输入所在层(1，顶层；2，底层；3，顶层丝印层；4，底层丝印层；5，顶层焊盘层；6，底层焊盘层；7，顶层阻焊层；8，底层阻焊层；10，边框层；11，文档层): '));
+sourcepath = input('请输入源文件路径(示例：C:\\Users\\sora\\Desktop\\): ');
+sourcename = input('请输入源文件名称(示例：test4.bmp): ');
+invert_f = int(input('图像取反?(0,不取反; 1,取反): '));
 print('X最大尺寸: %d' % x_size);
 print('y最大尺寸: %d' % y_size);
 print('所在层: %d' % layer);
@@ -66,10 +75,10 @@ print("阈值：", ret);
 #img = img.transpose();
 ## 将图片处理成LCEDA文件
 # 图片-> 线段
-#   o------>x (col)     .---0-->.
+#   o------>x (col)     o---0-->o
 #   |                   ^       |1
 #   |                 3 |       v
-#   v                   .<--2---.
+#   v                   o<--2---o
 #   y (raw)
 # lines_mil(start_x, start_y, end_x, end_y)
 # lines_mil(start_col, start_raw, end_col, end_raw)
